0f4590e4ed0424712910aecb58655ae49d50e0a2
Ippili Aashish <ippiliaashish@Ippilis-MacBook-Pro.local>
2019-10-11 16:33:37 -0500

    add description

--- LRUCache.java
+++ LRUCache.java
@@ -1,11 +1,14 @@
 class LRUCache {
 
-    
+    //LRU Cache: It defines least recently used cache algorightm which is used very commonly for cache replacement algorithms. For this solution in order to acheive get and put operations in O(1) time complexity we need to use hashmap to store the node for the key.
+    // We need doubly linked list as it as track of prev and next node and it will easy to remove and update the head as necessary. 
+    //we need to additional function apart from get and put i.e removeNode to remove a node and addhead to add the particular/node to head.
     Node head;
     Node tail;
     Map<Integer, Node> map;
     int capacity;
     int size;
+
     public LRUCache(int capacity) {
         head = new Node(0,0);
         tail = new Node(0,0);
@@ -17,6 +20,7 @@ class LRUCache {
     }
     
     public int get(int key) {
+        //When ever we perform a get operation we need to check if it exists in the map. if so then change the position of the node to head as it is recently used.
         if(map.containsKey(key)){
             Node node = map.get(key);
             removeNode(key);
@@ -29,6 +33,7 @@ class LRUCache {
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
+            //If we are trying to put the same value again then also we need to change the position of that node onto the head as it is recently used node.
             Node node = map.get(key);
             removeNode(key);
             addHead(key, value);
@@ -46,11 +51,11 @@ class LRUCache {
         node.prev = head;
         map.put(key, node);
         size++;
+        //we need to check the size of the node if it is more than capacity then we should remove tail.
          if (size > capacity) {
             Node preTail = tail.prev;
             removeNode(preTail.key);
         }
-
     }
     
     public void removeNode(int key){
